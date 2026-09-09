c6c116fe2e15d924bc4ccefc974f78682d88ec2f
Lokesh Goyal <lokeshgoyal94615@gmail.com>
2026-09-09 19:41:51 +0530

    made dashboard-header

--- src/components/ui/wavy-background.tsx
+++ src/components/ui/wavy-background.tsx
@@ -13,6 +13,7 @@ export const WavyBackground = ({
   blur = 10,
   speed = "fast",
   waveOpacity = 0.5,
+  waveYOffset = 250,
   ...props
 }: {
   children?: any;
@@ -24,6 +25,7 @@ export const WavyBackground = ({
   blur?: number;
   speed?: "slow" | "fast";
   waveOpacity?: number;
+  waveYOffset?: number;
   [key: string]: any;
 }) => {
   const noise = createNoise3D();
@@ -76,7 +78,7 @@ export const WavyBackground = ({
       ctx.strokeStyle = waveColors[i % waveColors.length];
       for (x = 0; x < w; x += 5) {
         var y = noise(x / 800, 0.3 * i, nt) * 100;
-        ctx.lineTo(x, y + h * 0.5); // adjust for height, currently at 50% of the container
+        ctx.lineTo(x, y + waveYOffset); 
       }
       ctx.stroke();
       ctx.closePath();
