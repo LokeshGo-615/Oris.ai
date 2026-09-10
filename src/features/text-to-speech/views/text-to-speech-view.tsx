3561ae1d01d913f9643ec437ccba9c9d53069da8
Lokesh Goyal <lokeshgoyal94615@gmail.com>
2026-09-10 17:53:29 +0530

    formed 3-panel page layout

--- src/features/text-to-speech/views/text-to-speech-view.tsx
+++ src/features/text-to-speech/views/text-to-speech-view.tsx
@@ -1,5 +1,6 @@
 import { TextInputPanel } from "../components/text-input-panel"
 import { VoicePreviewPlaceholder } from "../components/voice-preview-placeholder"
+import { SettingsPanel } from "../components/settings-panel"
 
 export function TextToSpeechView(){
     return(
@@ -7,9 +8,8 @@ export function TextToSpeechView(){
             <div className="flex min-h-0 flex-1 flex-col">
                 <TextInputPanel/>
                 <VoicePreviewPlaceholder/>
-
             </div>
-
+                 <SettingsPanel/>
         </div>
     )
 }
