14002db00e3294739c1aa0abd2786960cc403ede
Lokesh Goyal <lokeshgoyal94615@gmail.com>
2026-09-25 11:17:22 +0530

    worked on voice selection system

--- src/app/(dashboard)/text-to-speech/page.tsx
+++ src/app/(dashboard)/text-to-speech/page.tsx
@@ -1,7 +1,22 @@
- import { TextToSpeechView } from "@/features/text-to-speech/views/text-to-speech-view";
-import type { Metadata } from "next"
+import type { Metadata } from "next";
+import { TextToSpeechView } from "@/features/text-to-speech/views/text-to-speech-view";
+import { trpc, HydrateClient, prefetch } from "@/trpc/server";
 
- export const metadata:Metadata = {title : "Text to Speech"};
- export default function TextToSpeechPage(){
-    return <TextToSpeechView/>
-}
+export const metadata: Metadata = { title: "Text to Speech" };
+
+export default async function TextToSpeechPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ text?: string; voiceId?: string }>;
+}) {
+  const { text, voiceId } = await searchParams;
+
+  prefetch(trpc.voices.getAll.queryOptions());
+ // prefetch(trpc.generations.getAll.queryOptions());
+
+  return (
+    <HydrateClient>
+      <TextToSpeechView initialValues={{ text, voiceId }} />
+    </HydrateClient>
+  );
+};
